b65d8297a8f8f0621659b7b6fa87d83d43361329
SemVlad <s.vov.sem@gmail.com>
2022-02-22 21:48:05 +0200

    final

--- HW_4/src/Hypotenuse.java
+++ HW_4/src/Hypotenuse.java
@@ -3,22 +3,28 @@ import java.util.Scanner;
 public class Hypotenuse {
     public static void main(String[] args) {
 
-        // Ввод данных
-        System.out.print("Введите катет а: ");
-        Scanner scan = new Scanner(System.in);
-        int katA = scan.nextInt();
-        System.out.print("Введите катет b: ");
-        int katB = scan.nextInt();
+        // Объявляем переменные
         double hypotenuse;
+        int katA;
+        int katB;
 
-        // Тело программы
-        // Проверка вводных данных
-        if (katA <= 0 || katB <= 0) {
-            System.out.println("Введены не корректные данные. ");
-            System.out.print("Введите целые положительные значения больше нуля. ");
-        } else {
-            hypotenuse = Math.sqrt(katA * katA + katB * katB);
-            System.out.println("Гипотенуза = " + hypotenuse);
-        }
+        // Вводим данные
+        do {
+            System.out.print("Введите катет а: ");
+            Scanner scan = new Scanner(System.in);
+            katA = scan.nextInt();
+            System.out.print("Введите катет b: ");
+            katB = scan.nextInt();
+            // Проверка вводных данных
+            if (katA <= 0 || katB <= 0) {
+                System.out.println("Введены не корректные данные. ");
+                System.out.println("Введите целые положительные значения больше нуля. ");
+            }
+        } while (katA <= 0 || katB <= 0);
+        // Вычисление гипотенузы
+        hypotenuse = Math.sqrt(katA * katA + katB * katB);
+        System.out.println("Гипотенуза = " + hypotenuse);
     }
 }
+
+
